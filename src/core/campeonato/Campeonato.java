3babf66365419ec698a7dea642d509b761c3dace
Wanqui224 <juancamilo22408@gmail.com>
2026-04-08 21:01:44 -0500

    Añadir clases correspondientes

---
+++ src/core/campeonato/Campeonato.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core.campeonato;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Wanki
+ */
+public class Campeonato {
+    private ArrayList<juego> juegos; 
+
+    public Campeonato(ArrayList<juego> juegos) {
+        this.juegos = juegos;
+    }
+    
+}
